8aad7475cc841210c6e534aa8793260514e3d6bd
sam_arbid <samarbid13@users.noreply.github.com>
2019-11-05 15:28:28 +0100

    implement delete function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("userForm");
 const table = document.getElementById("userTable");
 
+// create import Json Data with XMLHttpRequest
+
 const tableBody = document.querySelector("#userTable > tbody");
 
 function loadJsonData() {
@@ -12,14 +14,17 @@ function loadJsonData() {
       const json = JSON.parse(req.responseText);
       populateData(json);
     } catch (e) {
-      console.warn("Could not load JSON! 😐");
+      console.warn(e + "/ Could not load JSON! 😐");
     }
   };
   req.send();
 }
+document.addEventListener("DOMContentLoaded", () => {
+  loadJsonData();
+});
 
 function populateData(json) {
-  // console.log(json);
+  console.log(json);
 
   // clear existing HTML
   while (tableBody.firstChild) {
@@ -40,22 +45,74 @@ function populateData(json) {
   });
 }
 
+//JQuery
+
 $(document).ready(function() {
   // add specific classes to every cell
 
-  const indexClass = "text-left w-1/12 p-2 pt-0";
-  const nameClass = "text-left w-3/12 p-2 pt-0";
-  const ageClass = "text-left w-2/12 p-2 pt-0";
-  const emailClass = "text-left w-4/12 p-2 pt-0";
+  const indexClass = "border-t w-1/12 p-2";
+  const nameClass = "border-t w-3/12 p-2";
+  const ageClass = "border-t w-2/12 p-2";
+  const emailClass = "border-t w-2/12 p-2";
 
-  $("tr > td:first-child").addClass(indexClass);
+  $("tr td:nth-child(1)").addClass(indexClass);
   $("tr td:nth-child(2)").addClass(nameClass);
   $("tr td:nth-child(3)").addClass(ageClass);
   $("tr td:nth-child(4)").addClass(emailClass);
 
+  // append svg Icons
 
+  $("tr").append("<td></td>");
+  $("tr td:nth-child(5)").addClass(emailClass);
+  $("tr td:nth-child(5)").append("<div></div>");
+  $("td div").addClass("flex justify-left");
+  $("td div").append("<a></a>");
+  $("td div").append("<a></a>");
+  $("div a:nth-child(1)").addClass("mr-2");
+  $("div a:nth-child(1)").attr("href", "#");
+  $("div a:nth-child(2)").attr("href", "#");
+  $("div a:nth-child(2)").attr("id", "trash");
+  $("div a:nth-child(1)").append(`<svg
+                   
+                      class="w-5 h-5 stroke-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke-width="1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-edit-3"
+                    >
+                      <path d="M12 20h9"></path>
+                      <path
+                        d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
+                      ></path>
+                    </svg>`);
+
+  $("div a:nth-child(2)").append(`<svg
+                      class="w-5 h-5 stroke-current"
+                      
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke-width="1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-trash-2"
+                    >
+                      <polyline points="3 6 5 6 21 6"></polyline>
+                      <path
+                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+                      ></path>
+                      <line x1="10" y1="11" x2="10" y2="17"></line>
+                      <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>`);
+
+  // delete button function 
+  $("#userTable").on("click", "#trash", function() {
+    $(this).parent().parent().parent().remove();
+  });
+
+  
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadJsonData();
-});
